24bcb80da8ab4710d4ac744bb98d402c5d221f0f
DS7XD <lucasmpp7@gmail.com>
2024-11-01 10:15:21 -0300

    Update page.tsx

--- src/app/Guincho/page.tsx
+++ src/app/Guincho/page.tsx
@@ -4,8 +4,9 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import styles from './Guincho.module.css';
+import Link from 'next/link';
 
-const defaultIcon = L.icon({
+const iconePadrao = L.icon({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
@@ -15,67 +16,83 @@ const defaultIcon = L.icon({
   shadowSize: [41, 41],
 });
 
-const GeolocationApp: React.FC = () => {
+const Guincho: React.FC = () => {
+  useEffect(() => {
+    document.title = "Chamando Guincho - DiagnosCAR";
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = '/img/Logos/Diagnoscar.ico';
+    document.head.appendChild(link);
+  }, []);
 
-    useEffect(() => {
-        document.title = "Chamando Guincho - DiagnosCAR";
-        const link = document.createElement('link');
-        link.rel = 'icon';
-        link.href = '/img/Logos/Diagnoscar.ico';
-        document.head.appendChild(link);
-    }, []);
-
-
-
-  const [location, setLocation] = useState<{ latitude: number | null; longitude: number | null }>({
+  const [localizacao, setLocalizacao] = useState<{ latitude: number | null; longitude: number | null }>({
     latitude: null,
     longitude: null,
   });
-  
-  const [error, setError] = useState<string | null>(null);
 
-  const getLocation = () => {
+  const [endereco, setEndereco] = useState<string | null>(null);
+  const [erro, setErro] = useState<string | null>(null);
+  const [localizacaoObtida, setLocalizacaoObtida] = useState<boolean>(false); 
+
+  const obterLocalizacao = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          });
-          setError(null);
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+
+          setLocalizacao({ latitude, longitude });
+          setErro(null);
+          setLocalizacaoObtida(true); 
+
+          fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`)
+            .then(response => response.json())
+            .then(data => {
+              const { road, suburb, city, state, postcode } = data.address;
+
+              if (road) {
+                setEndereco(`${road || ''}, ${suburb || ''}, ${city || ''}, ${state || ''}, ${postcode || ''}`);
+              } else if (postcode) {
+                // Se não retornar o nome da rua, usar o CEP para buscar no ViaCEP
+                fetch(`https://viacep.com.br/ws/${postcode}/json/`)
+                  .then(response => response.json())
+                  .then(dataViaCEP => {
+                    if (!dataViaCEP.erro) {
+                      const { logradouro, bairro, localidade, uf } = dataViaCEP;
+                      setEndereco(`${logradouro || ''}, ${bairro || ''}, ${localidade || ''}, ${uf || ''}`);
+                    } else {
+                      setErro('Endereço não encontrado.');
+                    }
+                  })
+                  .catch(() => setErro('Erro ao obter endereço pelo ViaCEP.'));
+              } else {
+                setErro('Erro ao obter endereço. Tente novamente.');
+              }
+            })
+            .catch(() => setErro('Erro ao obter endereço. Tente novamente.'));
         },
         (error) => {
-          setError('Erro ao pegar localização, tente novamente.\n' + error.message);
+          setErro('Erro ao obter localização, tente novamente.\n' + error.message);
         }
       );
     } else {
-      setError('Geolocalização não é suportada pelo navegador.');
+      setErro('Geolocalização não é suportada pelo navegador.');
     }
-  };
-
-  const confirmado = () => {
-    window.alert("Guincho sendo enviado");
-    window.location.href = "obrigado.html";
-  };
-
-  const cancelado = () => {
-    window.location.href = "inicio.html";
   };
 
   return (
     <div className={styles.container}>
-      <h1>Geolocalização do Usuário</h1>
-      <button onClick={getLocation} className={styles.buttons}>Pegar Localização</button>
-      
-      {error && <p className={styles.error}>{error}</p>}
+      <h1>Sua atual localização: </h1>
+      {!localizacaoObtida && (
+        <button onClick={obterLocalizacao} className={styles.buttons}>Obter Localização</button>
+      )}
 
-      {location.latitude && location.longitude ? (
+      {erro && <p className={styles.error}>{erro}</p>}
+
+      {localizacao.latitude && localizacao.longitude ? (
         <div>
-          <p>Latitude: {location.latitude}</p>
-          <p>Longitude: {location.longitude}</p>
-
           <MapContainer
-            center={[location.latitude, location.longitude]}
+            center={[localizacao.latitude, localizacao.longitude]}
             zoom={13}
             className={styles['leaflet-container']}
           >
@@ -84,19 +101,33 @@ const GeolocationApp: React.FC = () => {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
             <Marker 
-              position={[location.latitude, location.longitude]}
-              icon={defaultIcon}
+              position={[localizacao.latitude, localizacao.longitude]}
+              icon={iconePadrao}
             >
               <Popup>
-                Você está aqui: [{location.latitude}, {location.longitude}]
+                Você está aqui: [{localizacao.latitude}, {localizacao.longitude}]
               </Popup>
             </Marker>
           </MapContainer>
 
+          {endereco ? (
+            <h3 className={styles.endereco}>Endereço: {endereco}</h3>
+          ) : (
+            <p>Carregando endereço...</p>
+          )}
+
+          <p className={styles.coordenadas}>
+            Coordenadas: [{localizacao.latitude}, {localizacao.longitude}]
+          </p>
+
           <div className={styles['buttons-container']}>
-            <h1>Enviar Guincho ?</h1>
-            <button onClick={confirmado} className={styles.buttons}>Enviar</button>
-            <button onClick={cancelado} className={styles.buttons}>Cancelar</button>
+            <h1>Enviar Guincho?</h1>
+            <Link href="/ObrigadoPorUsar">
+              <button className={styles.buttons}>Enviar</button>
+            </Link>
+            <Link href="/Pre-Diagnostico">
+              <button className={styles.buttons}>Cancelar</button>
+            </Link>
           </div>
         </div>
       ) : (
@@ -106,4 +137,4 @@ const GeolocationApp: React.FC = () => {
   );
 };
 
-export default GeolocationApp;
+export default Guincho;
